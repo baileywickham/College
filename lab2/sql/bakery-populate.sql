48a6a8620f552a1101d424d3995f0bb3cb367c7e
baileywickham <baileywickham@gmail.com>
2020-08-08 14:16:43 -0700

    rename to fit to format

--- lab2/sql/bakery-populate.sql
+++ lab2/sql/bakery-populate.sql
@@ -1,38 +1,3 @@
-DROP TABLE IF EXISTS wickham.items;
-DROP TABLE IF EXISTS wickham.receipts;
-
-DROP TABLE IF EXISTS wickham.customers;
-DROP TABLE IF EXISTS wickham.goods;
-
-create table customers (
-    CustId int primary key,
-    FirstName varchar(100),
-    LastName varchar(100)
-);
-
-create table goods(
-    Item varchar(100) unique,
-    Food varchar(100),
-    Flavor varchar(100),
-    Price decimal(10,2),
-    Primary key (Food, Flavor)
-);
-
-create table receipts (
-    ReceiptNum int primary key,
-    CustId int,
-    ReceiptDate date,
-    foreign key (CustId) references customers(CustId)
-);
-
-Create table items (
-    ReceiptNum varchar(100),
-    ReceiptLine int,
-    Item varchar(100),
-    primary key (ReceiptNum, ReceiptLine),
-    foreign key (item) references goods(Item)
-);
-
 INSERT INTO customers (CustId,LastName,FirstName) VALUES (13,'ARNN','KIP');
 INSERT INTO customers (CustId,LastName,FirstName) VALUES (16,'CRUZEN','ARIANE');
 INSERT INTO customers (CustId,LastName,FirstName) VALUES (10,'DUKELOW','CORETTA');
@@ -156,4 +121,3 @@ INSERT INTO items (ReceiptNum,ReceiptLine,Item) VALUES (64451,3,'24-8x10');
 INSERT INTO items (ReceiptNum,ReceiptLine,Item) VALUES (64451,4,'90-BLK-PF');
 INSERT INTO items (ReceiptNum,ReceiptLine,Item) VALUES (21622,1,'45-CO');
 INSERT INTO items (ReceiptNum,ReceiptLine,Item) VALUES (35011,1,'50-CHS');
-
--- lab2/sql/custom-populate.sql
+++ lab2/sql/custom-populate.sql
@@ -1,27 +1,3 @@
-DROP TABLE IF EXISTS wickham.locations;
-DROP TABLE IF EXISTS wickham.GIS;
-DROP TABLE IF EXISTS wickham.stores;
-
-create table stores (
-id int primary key
-);
-
-create table locations (
-storeid int,
-state varchar(100),
-location varchar(100),
-address varchar(100),
-primary key (state, location, address),
-foreign key (storeid) references stores(id)
-);
-
-create table GIS (
-storeid int,
-latitude varchar(100),
-longitude varchar(100),
-foreign key (storeid) references stores(id),
-primary key (latitude, longitude)
-);
 
 INSERT INTO stores (id) VALUES (0);
 INSERT INTO stores (id) VALUES (1);
@@ -246,4 +222,3 @@ INSERT INTO locations (storeid, latitude,longitude) VALUES (57, '33.640372836488
 INSERT INTO locations (storeid, latitude,longitude) VALUES (58, '33.714657','-112.1125722');
 INSERT INTO locations (storeid, latitude,longitude) VALUES (59, '33.66665768531911','-112.12322165701374');
 INSERT INTO locations (storeid, latitude,longitude) VALUES (60, '33.5814625','-112.1251124');
-
