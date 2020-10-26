b8fa0d167cafc415eb03a61aebcec11fb9220589
Bailey Wickham <baileywickham@gmail.com>
2020-10-25 22:28:32 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #scp -o RequestTTY=force -r /home/y/workspace/357 wickham@unix1.csc.calpoly.edu:/home/wickham
+DIR=csc453
 git add -A && git commit -am "automated deploy by deploy.sh" && git push
-ssh -t calPoly "cd 357; git fetch --all; git reset --hard origin/master;"
+ssh -t calpoly "cd ${DIR}; git fetch --all; git reset --hard origin/master;"
