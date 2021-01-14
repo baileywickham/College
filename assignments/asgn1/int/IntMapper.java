3dfff7d98738926e6f6ba48466dab2e5c243eb1d
baileywickham <baileywickham@gmail.com>
2021-01-13 19:12:52 -1000

    automated deploy by deploy.sh

--- assignments/asgn1/int/IntMapper.java
+++ assignments/asgn1/int/IntMapper.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.Mapper.*;
 import org.apache.log4j.Logger;
 
-public class TemperatureMapper extends 
+public class TemperatureMapper extends
           Mapper<LongWritable, Text, Text, IntWritable> {
    private static Logger THE_LOGGER = Logger.getLogger(AverageTemperatureDriver.class);
 
@@ -13,12 +13,7 @@ public class TemperatureMapper extends
          throws IOException, InterruptedException {
       THE_LOGGER.debug("I AM IN LOGGER");
       String valueAsString = value.toString().trim();
-      String[] tokens = valueAsString.split(" ");
-      if (tokens.length != 2) {
-         return;
-      }
-      context.write(new Text(tokens[0]), new IntWritable(Integer.parseInt(tokens[1])));
-     //single write, but we can have multiple writes to context
+      context.write(new IntWritable(Integer.parseInt(valueAsString)), new IntWritable(Integer.parseInt(tokens[1])));
    }
 }
 
