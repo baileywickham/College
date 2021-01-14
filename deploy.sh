30d47bc467d07098d6a177f9ae26e852088db43d
baileywickham <baileywickham@gmail.com>
2021-01-13 14:27:57 -1000

    automated deploy by deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 #scp -o RequestTTY=force -r /home/y/workspace/357 wickham@unix1.csc.calpoly.edu:/home/wickham
 DIR=CSC369
 git add -A && git commit -am "automated deploy by deploy.sh" && git push
-ssh -t node5 "cd ${DIR}; git fetch --all; git reset --hard origin/master;"
+ssh -t node5 "cd ${DIR}; git fetch --all; git reset --hard origin/main;"
