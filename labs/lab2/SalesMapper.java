c810831ef4dffe72d08b9ece6ea0f0fef61ad992
baileywickham <baileywickham@gmail.com>
2021-01-18 13:16:53 -1000

    automated deploy by deploy.sh

--- labs/lab2/SalesMapper.java
+++ labs/lab2/SalesMapper.java
@@ -7,12 +7,15 @@ import org.apache.log4j.Logger;
 public class SalesMapper extends
           Mapper<LongWritable, Text, Text, IntWritable> {
    private static Logger THE_LOGGER = Logger.getLogger(SalesDriver.class);
+   private final static IntWritable one = new IntWritable(1);
 
    @Override
    public void map(LongWritable key, Text value, Context context)
-         throws IOException, InterruptedException {
+         throws IOException, InterruptedException
+     {
 
       THE_LOGGER.debug("I AM IN LOGGER");
+
 
       String valueAsString = value.toString().trim();
       String[] tokens = valueAsString.split(" ");
@@ -20,7 +23,7 @@ public class SalesMapper extends
       if (tokens.length != 5) {
          return;
       }
-      context.write(new Text(tokens[1]), null);
+      context.write(new Text(tokens[1]), one);
    }
 }
 
