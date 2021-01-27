f29c73868073b0d0277225db7a89154b5b24603c
baileywickham <baileywickham@gmail.com>
2021-01-26 19:43:11 -1000

    automated deploy by deploy.sh

--- labs/lab3/CountDates.java
+++ labs/lab3/CountDates.java
@@ -2,24 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class CountDates {
-    public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws Exception {
+        HashMap<String, ArrayList<String>> hm = new HashMap<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader("./sales"))) {
             String line0;
             while ((line0 = br.readLine()) != null) {
-                String[] tokens = line0.split(" ");
-                if (map.containsKey(tokens[1])) {
-                    map.put(tokens[1], map.get(tokens[1]) + 1);
-                } else {
-                    map.put(tokens[1], 1);
+                String[] tokens = line0.split(",");
+                if (!hm.containsKey(tokens[1])) {
+                    hm.put(tokens[1], new ArrayList<String>());
                 }
+                hm.get(tokens[1]).add(tokens[2] + ", " + tokens[3]);
             }
         } catch (Exception e) {
             System.out.println(e);
         }
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.print(entry.getKey() + " ");
-            System.out.println(entry.getValue());
+        ArrayList<String> s = new ArrayList<String>(hm.keySet());
+        Collections.sort(s);
+        FileWriter f = new FileWriter("out");
+        for (String d : s) {
+            Collections.sort(hm.get(d));
+            System.out.println(d.toString() + "\t" + String.join(", ", hm.get(d) + "\n"));
         }
+        f.close();
     }
 }
