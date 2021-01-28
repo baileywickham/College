d19fa11c711be9074d376d594f66b5dc00d8edce
chelojimenez <marcelojimenezrocabado@gmail.com>
2021-01-28 01:00:54 -0400

    Not all instructions save

--- labs/lab2/src/Main.java
+++ labs/lab2/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Parser p = new Parser();
         for (String line : p.firstPass(p.fileToString(args[0]))) {
-            // System.out.println(line);
-            p.parseLine(line);
+            System.out.println(line);
+            Instruction i = p.parseLine(line);
+            System.out.println(i);
         }
         // System.out.println(Arrays.asList(p.labels));
         // System.out.println(p.parseR("add$v0,$v0,$0"));
