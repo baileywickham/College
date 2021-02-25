ee34c7cdbbf112928baded37f1ee26a06fe91d03
baileywickham <baileywickham@gmail.com>
2021-02-24 14:47:39 -1000

    add lab4

---
+++ labs/lab4/src/lab4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class lab4 {
+    public static void main(String[] args) {
+        Interpreter i = new Interpreter(args[0]);
+        if (args.length == 2) {
+            String[] lines = Parser.fileToString(args[1]).split("\n");
+            for (String line : lines) {
+                System.out.printf("mips> %s\n", line);
+                i.parseCmd(line.split(" "));
+            }
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.printf("mips> ");
+                String tokens[] = scanner.nextLine().split(" ");
+                if (tokens[0].equals("q")) {
+                    return;
+                }
+                i.parseCmd(tokens);
+            }
+
+        }
+    }
+}
