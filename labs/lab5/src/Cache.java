9464105abd7430e2f1da8c4ec14b308e074a9897
baileywickham <baileywickham@gmail.com>
2021-03-11 19:12:11 -1000

    automated deploy by deploy.sh
    Former-commit-id: a8d36d5671d3c21bcf690b7afeff23a4f8637be2

--- labs/lab5/src/Cache.java
+++ labs/lab5/src/Cache.java
@@ -1,31 +1,30 @@
 import java.math.BigInteger;
-import java.math.*;
 
 public class Cache {
     //long twotothethirtysecond = 4294967296;
-    CacheEntry[] cache;
+    CacheEntry[][] cache;
     int hits = 0;
     int reads = 0;
     int assoc;
     int blockSize;
     int cacheSize;
-    int cashNumber;
+    int cacheNumber;
     int indexSize;
     
     public float getRatio(){
         return (float) hits / (float) reads;
     }
 
-    public Cache(int casheNumber, int cacheSize, int assoc, int blockSize) {
+    public Cache(int cacheNumber, int cacheSize, int assoc, int blockSize) {
         this.reads=0;
         this.cacheSize = cacheSize;
         this.assoc = assoc;
         this.blockSize = blockSize;
-        this.cashNumber = casheNumber;
-        this.indexSize = (cacheSize / (this.blockSize * 4));
-        allocCache(indexSize);
+        this.cacheNumber = cacheNumber;
+        this.indexSize = (cacheSize / (this.blockSize * 4)) / assoc;
+        allocCache(indexSize, assoc);
     }
-    public void lookup(String addr) {
+    public void lookup(String addr, int lineNum) {
 
         // [tag, index, block, byte 2b]
         // [tag, index, block, byte 2b]
@@ -36,19 +35,35 @@ public class Cache {
         String binary = hexToBin(addr);
         String tag = binary.substring(0, tagSize);
         int index = Integer.parseInt(binary.substring(tagSize, tagSize + indexBits),2);
-        CacheEntry possible = cache[index % indexSize];
-        if (possible.valid && possible.tag.equals(tag)) {
-            hits++;
-        } else {
-            cache[index % indexSize].tag = tag;
-            cache[index % indexSize].valid = true;
+        CacheEntry _min = cache[index % indexSize][0];
+        int _minIndex = 0;
+        for (int i = 0; i < this.assoc; i++) {
+            CacheEntry possible = cache[index % indexSize][i];
+            if (possible.lineNum < _min.lineNum) {
+                _minIndex = i;
+                _min = possible;
+            }
+            if (!possible.valid) {
+                cache[index % indexSize][i].tag = tag;
+                cache[index % indexSize][i].valid = true;
+                cache[index % indexSize][i].lineNum = lineNum;
+                return;
+            }
+            if (possible.tag.equals(tag)) {
+                cache[index % indexSize][i].lineNum = lineNum;
+                hits++;
+                return;
+            }
         }
+        cache[index % indexSize][_minIndex].tag = tag;
+        cache[index % indexSize][_minIndex].valid = true;
+        cache[index % indexSize][_minIndex].lineNum = lineNum;
     }
     public int log2(int i) {
         return (int) (Math.log(i) / Math.log(2));
     }
     public void printResults() {
-        System.out.printf("Cache #%d\n", cashNumber);
+        System.out.printf("Cache #%d\n", cacheNumber);
         System.out.printf("Cache size: %dB	Associativity: %d	Block size: %d\n", cacheSize, assoc, blockSize); 
         System.out.printf("Hits: %d	Hit Rate: %.2f%%\n", hits, 100*getRatio());
         System.out.println("---------------------------");
@@ -62,19 +77,21 @@ public class Cache {
         if (i.signum() < 0) {
             ext = "1";
         }
-        String s = i.toString(2);
+        StringBuilder s = new StringBuilder(i.toString(2));
         if (s.length() > n) {
             return s.substring(s.length()-n);
         }
         while (s.length() < n) {
-            s = ext + s;
+            s.insert(0, ext);
         }
-        return s;
+        return s.toString();
     }
-    public void allocCache(int n) {
-        this.cache = new CacheEntry[n];
+    public void allocCache(int n, int m) {
+        this.cache = new CacheEntry[n][m];
         for (int i = 0; i < n; i++) {
-            cache[i] = new CacheEntry(false, "");
+            for (int j = 0; j < m; j++) {
+                cache[i][j] = new CacheEntry(false, "", -1);
+            }
         }
     }
 }
@@ -82,8 +99,10 @@ public class Cache {
 class CacheEntry {
     boolean valid;
     String tag;
-    public CacheEntry(boolean valid, String tag) {
+    int lineNum;
+    public CacheEntry(boolean valid, String tag, int lineNum) {
         this.tag = tag;
         this.valid = valid;
+        this.lineNum = lineNum;
     }
 }
--- labs/lab5/src/lab5.java
+++ labs/lab5/src/lab5.java
@@ -6,7 +6,7 @@ import java.nio.file.Paths;
 
 public class lab5 {
     public static void main(String[] args) {
-        String file = fileToString(args[0]);
+        String[] lines = fileToString(args[0]).split("\n");
         Cache c0 = new Cache(1, 2048, 1, 1);
         Cache c1 = new Cache(2, 2048, 1, 2);
         Cache c2 = new Cache(3, 2048, 1, 4);
@@ -14,22 +14,22 @@ public class lab5 {
         Cache c4 = new Cache(5, 2048, 4, 1);
         Cache c5 = new Cache(6, 2048, 4, 4);
         Cache c6 = new Cache(7, 4096, 1, 1);
-        for (String line: file.split("\n")) {
-            String addr = line.split("\\s+")[1].trim();
-            c0.lookup(addr);
-            c1.lookup(addr);
-            c2.lookup(addr);
-            //c3.lookup(addr);
-            //c4.lookup(addr);
-            //c5.lookup(addr);
-            c6.lookup(addr);
+        for (int i = 0; i < lines.length; i++) {
+            String addr = lines[i].split("\\s+")[1].trim();
+            c0.lookup(addr, i);
+            c1.lookup(addr, i);
+            c2.lookup(addr, i);
+            c3.lookup(addr, i);
+            c4.lookup(addr, i);
+            c5.lookup(addr, i);
+            c6.lookup(addr, i);
         }
         c0.printResults();
         c1.printResults();
         c2.printResults();
-        //c3.printResults();
-        //c4.printResults();
-        //c5.printResults();
+        c3.printResults();
+        c4.printResults();
+        c5.printResults();
         c6.printResults();
     }
 
